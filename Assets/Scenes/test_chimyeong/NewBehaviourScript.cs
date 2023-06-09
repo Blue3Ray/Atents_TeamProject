f654fdff416c9c7bb7cddb42e77b0542f6407a82
TaeChichi <canon082828@gmail.com>
2023-06-09 15:21:22 +0900

    적 움직임
    123

---
+++ Assets/Scenes/test_chimyeong/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+       private void Awake()
+        {
+            rigid = GetComponent <Rigidbody2D>();
+        }
+        void FixeUpdate()
+        { 
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

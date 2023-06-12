1cfc0827c966bbeaba43f93b8c0c916e457b92ee
TaeChichi <canon082828@gmail.com>
2023-06-12 13:52:58 +0900

    인치명 커밋
    123213213

--- Assets/Scenes/test_chimyeong/NewBehaviourScript.cs
+++ Assets/Scenes/test_chimyeong/NewBehaviourScript.cs
@@ -1,27 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     Rigidbody2D rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-       private void Awake()
-        {
-            rigid = GetComponent <Rigidbody2D>();
-        }
-        void FixeUpdate()
-        { 
-
-        }
+       
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigid.velocity = new Vector2(-1,rigid.velocity.y); // 단순 왼쪽방향 이동
     }
 }

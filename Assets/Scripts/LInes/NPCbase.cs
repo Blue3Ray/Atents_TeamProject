79f972bb201b1d46e3286e3b40e83ce26ff13ea2
jisun <zzzsunrise@users.noreply.github.com>
2023-06-12 15:20:44 +0900

    mouse click with npc

--- Assets/Scripts/LInes/NPCbase.cs
+++ Assets/Scripts/LInes/NPCbase.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NPCbase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	ActionControl actionControle;
+	private float isClick;
+
+	private void Awake()
+	{
+		actionControle = new ActionControl();
+	}
+
+	private void OnEnable()
+	{
+		actionControle.ClickAction.Enable();
+		actionControle.ClickAction.Mouse_Left.performed += OnClick;
+		actionControle.ClickAction.Mouse_Left.canceled += OnClick;
+	}
+
+	private void OnDisable()
+	{
+		actionControle.ClickAction.Mouse_Left.canceled -= OnClick;
+		actionControle.ClickAction.Mouse_Left.performed -= OnClick;
+		actionControle.ClickAction.Disable();
+	}
+
+	private void OnClick(InputAction.CallbackContext _)
+	{
+		isClick = _.ReadValue<float>();
+		Debug.Log($"{isClick}");
+	}
 }

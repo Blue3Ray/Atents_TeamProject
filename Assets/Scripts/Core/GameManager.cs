9ff5eada6d589aea3d89f27df1d3f2ee1e087ac2
Blue3Ray <xoals1126@gmail.com>
2023-06-15 14:54:20 +0900

    임시 플레이어 클래스 추가

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -2,7 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// 테스트 용으로 만들어놓은 임시 Player 클래스 나중에 재민씨가 클래스 만들면 없앨것
+/// </summary>
+public class Player : MonoBehaviour
+{
+    int hp = 100;
+    public int HP
+    {
+        get { return hp; } set {  hp = value; }
+    }
+}
+
+
 public class GameManager : Singleton<GameManager>
 {
-    // 게임의 전반적인 데이터를 관리할 예정
+    Player player;
+
+    private void Awake()
+    {
+        player = new Player();
+    }
 }

9fde7dcd27a304ca71f7f1d1289f5e8a09abc568
김수영 <49178621+may721@users.noreply.github.com>
2023-06-16 15:18:15 +0900

    UI 마우스 이벤트 내용 추가

---
+++ Assets/Scripts/UI/MouseSelect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseSelect : MonoBehaviour
+{
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
+    
+}

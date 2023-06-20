ab83b00326d8e2fc67cea67d853d7ea8d7aa0165
inchimyeong <dlsclaud5@naver.com>
2023-06-20 15:09:44 +0900

    몬스터 플레이어 추적 시 부자연스러운 모션 고침, 몬스터 체력, 경험치 추가

--- Assets/Scripts/enemy/EnemyBase.cs
+++ Assets/Scripts/enemy/EnemyBase.cs
@@ -6,11 +6,28 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     public Transform player;
-    public Vector3 dir;
     public float speed;
     float range =20;
+    public float hp = 1000.0f;
+    public float maxhp = 1000.0f;
+    public float exp = 0;
 
     SpriteRenderer spriteRenderer;
+     // public Vector3(변수타입) dir(변수); <-변수문
+    public Vector3 dir;
+    Vector3 Dir
+    {
+        get { return dir; } // 값을 받으려고 할때 ,프로퍼티 문
+        set // 특정 조건에만 실행하는 
+        {           
+            dir = value;// 값을 넣으려고 할때 
+            if (dir.x < 0) spriteRenderer.flipX = false;
+            if (dir.x > 0) spriteRenderer.flipX = true;
+
+        } 
+    }
+    
+    
 
     private void Awake()
     {
@@ -20,32 +37,32 @@ public class EnemyBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        dir = Vector3.right;
+        Dir = Vector3.right;
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Time.deltaTime*dir*speed);
+        transform.Translate(Time.deltaTime*Dir*speed);
 
-        if ((player.position.x - transform.position.x)<range)
+        if ((player.position.x - transform.position.x)<range) //플레이어 추적
         {
-            dir = (player.position - transform.position);
-            dir.Normalize();
-        //  dir = (player.position < transform.position(spriteRenderer.flipX = false);
+            Dir = (player.position - transform.position); //플레이어 위치와 몬스터 위치를 통해 거리 계산
+            Dir.Normalize(); //플레이어 추적 시 부자연스럽게 따라오는 걸 방지하기 위해 속도 정함
+        //  Dir = (player.position < transform.position(spriteRenderer.flipX = false);
         }
         else
         {
             if (transform.position.x > 5)
             {
-                dir = Vector3.left;
-                spriteRenderer.flipX = false;
+                Dir = Vector3.left;
+                spriteRenderer.flipX = false; 
             }
             else if (transform.position.x < -5)
             {
-                dir = Vector3.right;
-                spriteRenderer.flipX = true;
+                Dir = Vector3.right;
+                spriteRenderer.flipX = true; // 왼쪽으로 최대한 간 뒤 오른쪽으로 돌아갔을때 자연스럽게 가기 위해 좌우반전을 줌
             }
         }
 

1f5319c67f93a1c4e0418ff692b7b562333dfb66
zzzsunrise <113669170+zzzsunrise@users.noreply.github.com>
2023-06-21 23:55:07 +0900

    이벤트별 대화 담기 (완)
    지금까지 안됐던 이유
    1. tmp 리스트 초기화 X
    2. break 한 곳만 잡아줌
    3. if를 while로 고침
    4. contextlist 초기화 X

--- Assets/Scripts/LInes/js_dialogue/DialogueParse.cs
+++ Assets/Scripts/LInes/js_dialogue/DialogueParse.cs
@@ -12,33 +12,16 @@ public class DialogueParse : MonoBehaviour
 
     List<OneDialogueEvent> finalDialogues;
 
-    int saveIndex;
-
-    int dialogueCount;
-
 
 	//List<Dialogue> dialoguesList;
 	//int tmpcount = 0;
 
 	public Dialogue[] Parse (string CSV_File)
     {
-        
-        //이 함수가 반환하는 건 GMDialogue의 배열이고
-        //여기선 GMDialogue들의 리스트 이다!
-        //List<Dialogue> dialoguesList = new List<Dialogue>();
-        
-        //TextAsset 타입으로 입력받은 이름의 csv 파일을 찾아서
-        //csvData라는 변수에 집어넣었다.
         TextAsset csvData = Resources.Load<TextAsset>(CSV_File);
 
-        //아까 가져온 파일을 {}안에 들어간 걸 기준으로 나눠서
-        //새로운 char배열 즉 csv에서 한 줄씩 일단 빼온다.
         string[] stringData = csvData.text.Split(new char[] { '\n' });
 
-		//dialogues = new List<Dialogue>();
-
-        //위에서 한 행 씩 쪼갰으니
-        //여기서는 그 행에서 콤마별로 쪼갠다.
         for(int i= 1; i < stringData.Length;)
         {
             
@@ -51,7 +34,6 @@ public class DialogueParse : MonoBehaviour
 			do
             {
                 contextList.Add(row[2]);
-				//Debug.Log($"두와일: {row[2]}");
 				if (++i < stringData.Length)
 				{
                     
@@ -67,7 +49,6 @@ public class DialogueParse : MonoBehaviour
 			dialogues.Add(temp);
 		}
 
-       // dialogueCount = dialogues.Count;
 		return dialogues.ToArray();
         
     }
@@ -80,8 +61,9 @@ public class DialogueParse : MonoBehaviour
 		{
 			OneDialogueEvent tmpOnDialogueEvent = new OneDialogueEvent();
 
-			if (dialogues[i].Event.ToString() != "")
+			while (dialogues[i].Event.ToString() != "")
 			{
+				tmpOnDialogueEvent = new OneDialogueEvent();
 				tmpOnDialogueEvent.EventName = dialogues[i].Event;
 				tmpOnDialogueEvent.EventDialogues = new List<Dialogue>();
 
@@ -96,9 +78,14 @@ public class DialogueParse : MonoBehaviour
 
 				} while (dialogues[i].Event.ToString() == "");
 
+				finalDialogues.Add(tmpOnDialogueEvent);
+				if (i >= dialogues.Count)
+				{
+					break;
+				}
+
 			}
 
-			finalDialogues.Add(tmpOnDialogueEvent);
 
 		}
 	}
@@ -115,25 +102,36 @@ public class DialogueParse : MonoBehaviour
 	{
         Parse("NewDialogue");
 		eventParse();
-		//for(int i = 0; i < finalDialogues.Count; i++)
-		//{
-		//	Debug.Log($"{finalDialogues[i].EventName}");
-		//	Debug.Log($"{finalDialogues[i].EventDialogues[0].contexts[0]}");
-
-		//}
-
-
-		for (int j = 0; j < dialogues.Count; j++)
+		
+		// *** 아래는 finalDialogues의 모든 요소를 디버그해주는 삼중중첩 반복문입니다.
+		//Test가 아닐 때는 꼭 가려주세요
+		for (int i = 0; i < finalDialogues.Count; i++)
 		{
-			Debug.Log($"{j}");
+			Debug.Log($"{finalDialogues[i].EventName}");
 
-			Debug.Log($"{dialogues[j].Event}");
-			Debug.Log($"{dialogues[j].name}");
-			for (int i = 0; i < dialogues[j].contexts.Length; i++)
+			for(int j = 0; j < finalDialogues[i].EventDialogues.Count; j++)
 			{
-				Debug.Log($"{dialogues[j].contexts[i]}");
+
+				for(int z = 0; z < finalDialogues[i].EventDialogues[j].contexts.Length; z++)
+				{
+					Debug.Log($"{finalDialogues[i].EventDialogues[j].contexts[z]}");
+				}
 			}
+
 		}
+
+		//*** 아래는 dialogues의 요소를 하나씩 디버그 해주는 코드입니다
+		//for (int j = 0; j < dialogues.Count; j++)
+		//{
+		//	Debug.Log($"{j}");
+
+		//	Debug.Log($"{dialogues[j].Event}");
+		//	Debug.Log($"{dialogues[j].name}");
+		//	for (int i = 0; i < dialogues[j].contexts.Length; i++)
+		//	{
+		//		Debug.Log($"{dialogues[j].contexts[i]}");
+		//	}
+		//}
 
 	}
 }

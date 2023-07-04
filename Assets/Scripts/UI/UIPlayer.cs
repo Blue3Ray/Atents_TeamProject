bfc0eb5ad419d47292c52150511deb838f3ef4e2
123 <133838215+Elce08@users.noreply.github.com>
2023-07-04 13:51:57 +0900

    ui수정

--- Assets/Scripts/UI/UIPlayer.cs
+++ Assets/Scripts/UI/UIPlayer.cs
@@ -18,31 +18,41 @@ public class UIPlayer : MonoBehaviour
 
     private void Awake()
     {
+        actionControl = new ActionControl();
+
         // UIPlayer 자식인 elemanterMenu 
         elemanterMenu = transform.GetChild(0).gameObject;
         
         // elemanterMenu 위치를 uiplayer 위치로 받아오기
-        elemanterMenu.transform.position = transform.position;
+       // elemanterMenu.transform.position = transform.position;
 
-        elemanter = GetComponent<ElemanterMenu>();
+        elemanter = new ElemanterMenu();
     }
 
-    //private void OnEnable()
-    //{
-    //    actionControl.MouseClickMenu.MouesEvent.Enable();
-    //    actionControl.MouseClickMenu.MouesEvent.performed += Onclick;
-    //}
+    private void OnEnable()
+    {
+        actionControl.MouseClickMenu.MouesEvent.Enable();
+        actionControl.MouseClickMenu.MouesEvent.performed += Onclick;
+        actionControl.MouseClickMenu.MouesEvent.canceled += ElemanterSelect;
+    }
 
-    //private void OnDisable()
-    //{
-    //    actionControl.MouseClickMenu.MouesEvent.performed -= Onclick;
-    //    actionControl.MouseClickMenu.MouesEvent.Disable();
-    //}
+ 
 
-    //private void Onclick(InputAction.CallbackContext _)
-    //{
-    //    elemanterMenu.SetActive(true);
-    //}
+    private void OnDisable()
+    {
+        actionControl.MouseClickMenu.MouesEvent.canceled -= ElemanterSelect;
+        actionControl.MouseClickMenu.MouesEvent.performed -= Onclick;
+        actionControl.MouseClickMenu.MouesEvent.Disable();
+    }
 
+    private void Onclick(InputAction.CallbackContext _)
+    {
+        elemanterMenu.SetActive(true);
+    }
+
+    private void ElemanterSelect(InputAction.CallbackContext _)
+    {
+        elemanter.ElemanterSelct();
+    }
 
 }

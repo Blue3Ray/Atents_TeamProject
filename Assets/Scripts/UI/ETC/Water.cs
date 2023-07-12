231c8ec3a9628b57168e2290b0080052ff31ee1c
123 <133838215+Elce08@users.noreply.github.com>
2023-07-12 14:20:16 +0900

    ui수정

---
+++ Assets/Scripts/UI/ETC/Water.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("¹° »ý¼º");
+    }
+}

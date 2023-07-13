e7cc3b279f11fced5380d64c00b84f0ba8d4ffc6
inchimyeong <125725323+steelfs@users.noreply.github.com>
2023-07-13 15:17:34 +0900

    몬스터에 체력바 붙였습니다

--- Assets/Scripts/enemy/Sword_Man.cs
+++ Assets/Scripts/enemy/Sword_Man.cs
@@ -2,18 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
 public class Sword_Man : MonoBehaviour
 {
     public GameObject objSwordMan;
     Animator animator;
+    public int maxHp;
+    //*주석처리 없엘 예정
+  //  public interface nowHp;
+ //   public interface atkDmg;
+    public float atkSpeed = 1;
+    public bool attacked = false;  
+  //  public Image nowHpbar;
 
-    // Start is called before the first frame update
-    void Start()
+    void AttackTrue()
+    {
+
+    }
+    void AttackFalse()
+    {
+
+    }
+    void SetAttackSpeed(float speed)
     {
         
-        animator = GetComponent<Animator>();
-        
     }
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        {
+
+            animator = GetComponent<Animator>();
+            maxHp = 50;
+        //    nowHp = 50;
+        //    atkDmg = 10;
+
+            transform.position = new Vector3(0, 0, 0);
+            animator = GetComponent<Animator>();
+        //    SetAttackSpeed(1.5)f
+        }
+
+   
 
     // Update is called once per frame
     void Update()

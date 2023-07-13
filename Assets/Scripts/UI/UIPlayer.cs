bf1b9ae4d67fb64bfb8c8ed2b5990d8f3dad846c
hw <qpwlwkd@naver.com>
2023-07-13 15:21:59 +0900

    ui 수정

--- Assets/Scripts/UI/UIPlayer.cs
+++ Assets/Scripts/UI/UIPlayer.cs
@@ -2,60 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 
 
 
 public class UIPlayer : MonoBehaviour
 {
-    ActionControl actionControl;
+    //ActionControl actionControl;
 
     // elematerMenu 객체 생성
    public  GameObject elemanterSlot;
 
-    ElemanterMenu elemanterMenu;
-
+    // ElemanterMenu elemanterMenu;
+    public Button elemanterbutton1;
+    public Button elemanterbutton2;
+    public Button elemanterbutton3;
+    public Button elemanterbutton4;
 
     private void Awake()
     {
-        actionControl = new ActionControl();
-
-        elemanterMenu = new ElemanterMenu();    
-
-        // UIPlayer 자식인 elemanterMenu 
-        
-        
-        // elemanterMenu 위치를 uiplayer 위치로 받아오기
-         //elemanterSlot.transform.position = transform.position;
-
+      
+        elemanterbutton1 = GetComponent<Button>();
         
     }
 
-    private void OnEnable()
+    
+
+    private void Update()
     {
-        actionControl.MouseClickMenu.MouesEvent.Enable();
-        actionControl.MouseClickMenu.MouesEvent.performed += Onclick;
-        actionControl.MouseClickMenu.MouesEvent.canceled += ElemanterSelect;
+        if (Input.GetMouseButtonDown(1))
+        {
+            elemanterSlot.SetActive(true);
+
+
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            Vector3 mousepostion = Mouse.current.position.ReadValue();
+            Debug.Log(mousepostion);
+
+           if(elemanterbutton1 != null )
+            {
+                elemanterbutton1.onClick.Invoke();
+                
+            }
+
+            elemanterSlot.SetActive(false);
+
+        }
     }
 
- 
+    
 
-    private void OnDisable()
-    {
-        actionControl.MouseClickMenu.MouesEvent.canceled -= ElemanterSelect;
-        actionControl.MouseClickMenu.MouesEvent.performed -= Onclick;
-        actionControl.MouseClickMenu.MouesEvent.Disable();
-    }
 
-    private void Onclick(InputAction.CallbackContext _)
-    {
-        elemanterSlot.SetActive(true);
-    }
-
-    private void ElemanterSelect(InputAction.CallbackContext context)
-    {
-        elemanterMenu.ElemanterSelct();
-       
-    }
 
 }

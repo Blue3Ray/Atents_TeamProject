d70eccabe45f2b4dc1b63f6a7bbbadd3c1ae573c
inchimyeong <125725323+steelfs@users.noreply.github.com>
2023-07-14 15:10:02 +0900

    image함수 적용안되서 임시 주석처리

--- Assets/Scripts/enemy/Enemy1.cs
+++ Assets/Scripts/enemy/Enemy1.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+/* 임시주석처리 
 
 public class Enemy1 : MonoBehaviour
 {
@@ -9,23 +10,61 @@ public class Enemy1 : MonoBehaviour
     public GameObject prfHpBar;
     public GameObject canvas;
     Animator animator;
-
     RectTransform hpBar;
+    public string enemyName;
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public int atkSpeed;
 
     public float height = 1.7f;
 
     // Start is called before the first frame update
     void Start()
     {
-        hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();  
+        hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
+        if (name.Equals("Enemy1"))
+        {
+            SetEnemyStatus("Enemy1", 100, 10, 1);        
+        }
+        nowHpbar = hpBar.transform.GetChild(0).GetComponent<Image>();
     }
+
+    private void SetEnemyStatus(string _enemyName, int _maxHp, int _atkDmg, int _atkSpeed)
+    {
+        enemyName = _enemyName;
+        maxHp = _maxHp;
+        nowHp = _maxHp;
+        atkDmg = +atkDmg;
+        atkSpeed = _atkSpeed;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (sword_man.attacked)
+            {
+                nowHp -= sword_man.atkDmg;
+                Debug.Log(nowHp);
+                sword_man.attacked = false;
+                if (nowHp <= 0) // 적 사망 
+                {
+                    Destroy(gameObject);
+                    Destroy(hpBar.gameObject);
+                }
+            }
+        }
+    }
+
+    public Sword_Man sword_man;
+    Image nowHpbar;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 _hpBarPos =
-            Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
+        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
         hpBar.position = _hpBarPos;
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -35,3 +74,4 @@ public class Enemy1 : MonoBehaviour
         }
     }
 }
+  */
--- Assets/Scripts/enemy/Sword_Man.cs
+++ Assets/Scripts/enemy/Sword_Man.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
+/* 임시주석처리 
 
 
 public class Sword_Man : MonoBehaviour
@@ -11,48 +11,52 @@ public class Sword_Man : MonoBehaviour
     public GameObject objSwordMan;
     Animator animator;
     public int maxHp;
-    //*주석처리 없엘 예정
-  //  public interface nowHp;
- //   public interface atkDmg;
+    public int nowHp;
+    public int atkDmg;
     public float atkSpeed = 1;
-    public bool attacked = false;  
-  //  public Image nowHpbar;
+    public bool attacked = false;
+    public Image nowHpbar;
 
     void AttackTrue()
     {
-
+        attacked = true;
     }
     void AttackFalse()
     {
-
+        attacked = false;
     }
     void SetAttackSpeed(float speed)
     {
-        
+        animator.SetFloat("attackSpeed", speed);
+        atkSpeed = speed;
     }
 
 
     // Start is called before the first frame update
 
     void Start()
+
+
     {
-        {
 
-            animator = GetComponent<Animator>();
-            maxHp = 50;
-        //    nowHp = 50;
-        //    atkDmg = 10;
+        animator = GetComponent<Animator>();
+        maxHp = 50;
+        nowHp = 50;
+        atkDmg = 10;
 
-            transform.position = new Vector3(0, 0, 0);
-            animator = GetComponent<Animator>();
-        //    SetAttackSpeed(1.5)f
-        }
+        transform.position = new Vector3(0, 0, 0);
+        animator = GetComponent<Animator>();
+        SetAttackSpeed(1.5f);
+    }
 
-   
+
 
     // Update is called once per frame
     void Update()
     {
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
+        if (Input.GetKey(KeyCode.RightArrow));
+        
         float h = Input.GetAxis("Horizontal");
         if (h > 0)
 
@@ -69,7 +73,7 @@ public class Sword_Man : MonoBehaviour
             transform.Translate(Vector3.left * Time.deltaTime);
         }
         else animator.SetBool("moving", false);
-        transform.Translate(new Vector3(h,0,0) * Time.deltaTime);
+        transform.Translate(new Vector3(h, 0, 0) * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.A) &&
             !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
@@ -77,4 +81,6 @@ public class Sword_Man : MonoBehaviour
             animator.SetTrigger("attack");
         }
     }
-}
+
+
+}   */

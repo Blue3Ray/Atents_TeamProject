7645958b033dbf7cf44a056e9eceb4877664736f
kim su young <qpwlwkd@naver.com>
2023-07-27 15:27:23 +0900

    UI 수정

--- Assets/Scripts/UI/RingMenu/RingMenu.cs
+++ Assets/Scripts/UI/RingMenu/RingMenu.cs
@@ -6,126 +6,30 @@ using UnityEngine.UI;
 using Unity.VisualScripting;
 using UnityEditor.SceneManagement;
 
-public class RingMenu : MonoBehaviour
+public class RingMenu
 {
-    float mousePointerY = 0.0f;
-    float mousePointerX = 0.0f;
+    // 링 메뉴 슬롯 기본 개수
+    public const int Default_RingMenu_Size = 4;
 
-    ActionControl acionControl;
+    RingMenuSlot[] slots;
 
-    // 속성 메뉴
-    Transform elemanterMenu;
+    public RingMenuSlot this[uint index] => slots[index]; 
+    
+    // 링메뉴 슬롯의 갯수
+    public int slotCount => slots.Length;
 
-    // 속성 메뉴 안에 있는 슬롯
-    Transform[] elemanterSlot;
+    TestPlayer owner;
+    public TestPlayer Owner => owner;
 
-    //속성 클래스
-    Fire fire;
-    Water water;
-    Wind wind;
-    Thunder thunder;
-
-    TestPlayer testPlayer;
-
-    private void Awake()
+    public RingMenu(TestPlayer owner, uint size = Default_RingMenu_Size)
     {
-        acionControl = new ActionControl();
-
-        // 링메뉴 자식인 속성 메뉴 호출
-        elemanterMenu = transform.GetChild(0);
-
-        // 속성 메뉴 자식인 각 속성 슬롯 호출 
-        elemanterSlot = new Transform[4];
-        elemanterSlot[0] = elemanterMenu.GetChild(0).transform;
-        elemanterSlot[1] = elemanterMenu.GetChild(1).transform;
-        elemanterSlot[2] = elemanterMenu.GetChild(2).transform;
-        elemanterSlot[3] = elemanterMenu.GetChild(3).transform;
-
-        // 초기값으로 속성 메뉴가 보이지 않는 상태
-        elemanterMenu.gameObject.SetActive(false);
-
-        // 각 속성 클래스 호출
-        fire= new Fire();
-        water = new Water();
-        wind = new Wind();
-        thunder= new Thunder();
-
-        testPlayer = FindObjectOfType<TestPlayer>();
-    }
-    private void OnEnable()
-    {
-        acionControl.MouseClickMenu.Enable();
-        acionControl.MouseClickMenu.MouesEvent.performed += OnElemanterMenu;
-        acionControl.MouseClickMenu.MouesEvent.canceled += OffElemanterMenu;
-    }
-
-
-
-    private void OnDisable()
-    {
-        acionControl.MouseClickMenu.MouesEvent.canceled -= OffElemanterMenu;
-        acionControl.MouseClickMenu.MouesEvent.performed -= OnElemanterMenu;
-        acionControl.MouseClickMenu.Disable();
-    }
-
-    private void Start()
-    {
-      
-    }
-
-    private void Update()
-    {
-        
-    }
-
-   // 마우스 오른쪽 버튼 누를 시 마우스 포인터 위치에 속성 메뉴 활성화
-    private void OnElemanterMenu(InputAction.CallbackContext context)
-    {
-        Vector3 mousePos = Mouse.current.position.ReadValue();
-        
-        elemanterMenu.position = mousePos;
-        elemanterMenu.gameObject.SetActive(true);
-
-    }
-
-    // 속성 메뉴 활성화 된 상태에서 속성메뉴 위치에서 마우스 오른쪽 버튼 해제 시 속성 선택
-    private void OffElemanterMenu(InputAction.CallbackContext context)
-    {
-        Vector3 mousePos = Mouse.current.position.ReadValue();
-        Vector3 target = context.ReadValue<Vector2>();
-        target = mousePos;
-
-       if(target.y > elemanterMenu.position.y)
+        slots= new RingMenuSlot[size];
+        for(uint i = 0; i < size; i++)
         {
-            fire.OnClickBuuton();
-            testPlayer.element = Element.Fire;
-            elemanterMenu.gameObject.SetActive(false);
-        }
-       else if (target.x > elemanterMenu.position.x)
-        {
-            water.OnClickBuuton();
-            testPlayer.element = Element.Water;
-            elemanterMenu.gameObject.SetActive(false);
-        }
-       else if(target.y < elemanterMenu.position.y)
-        {
-            wind.OnClickBuuton();
-            testPlayer.element = Element.Wind;
-            elemanterMenu.gameObject.SetActive(false);
-        }
-       else if (target.x < elemanterMenu.position.x)
-        {
-            thunder.OnClickBuuton();
-            testPlayer.element = Element.Thunder;
-            elemanterMenu.gameObject.SetActive(false);
-        }
-        else
-        {
-            testPlayer.element = Element.None;
-            elemanterMenu.gameObject.SetActive(false);
+            slots[i] = new RingMenuSlot(i);
         }
 
-        
+        this.owner = owner;
     }
 
 }
--- Assets/Scripts/UI/RingMenu/UI/RingMenuUI.cs
+++ Assets/Scripts/UI/RingMenu/UI/RingMenuUI.cs
@@ -1,18 +1,125 @@
+using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RingMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    ActionControl acionControl;
+
+    // 속성 메뉴
+    Transform elemanterMenu;
+
+    // 속성 메뉴 안에 있는 슬롯
+    Transform[] elemanterSlot;
+
+    //속성 클래스
+    Fire fire;
+    Water water;
+    Wind wind;
+    Thunder thunder;
+
+    TestPlayer testPlayer;
+
+    private void Awake()
     {
-        
+        acionControl = new ActionControl();
+
+        // 링메뉴 자식인 속성 메뉴 호출
+        elemanterMenu = transform.GetChild(0);
+
+        // 속성 메뉴 자식인 각 속성 슬롯 호출 
+        elemanterSlot = new Transform[4];
+        elemanterSlot[0] = elemanterMenu.GetChild(0).transform;
+        elemanterSlot[1] = elemanterMenu.GetChild(1).transform;
+        elemanterSlot[2] = elemanterMenu.GetChild(2).transform;
+        elemanterSlot[3] = elemanterMenu.GetChild(3).transform;
+
+        // 초기값으로 속성 메뉴가 보이지 않는 상태
+        elemanterMenu.gameObject.SetActive(false);
+
+        // 각 속성 클래스 호출
+        fire = new Fire();
+        water = new Water();
+        wind = new Wind();
+        thunder = new Thunder();
+
+        testPlayer = FindObjectOfType<TestPlayer>();
+    }
+    private void OnEnable()
+    {
+        acionControl.MouseClickMenu.Enable();
+        acionControl.MouseClickMenu.MouesEvent.performed += OnElemanterMenu;
+        acionControl.MouseClickMenu.MouesEvent.canceled += OffElemanterMenu;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnDisable()
     {
-        
+        acionControl.MouseClickMenu.MouesEvent.canceled -= OffElemanterMenu;
+        acionControl.MouseClickMenu.MouesEvent.performed -= OnElemanterMenu;
+        acionControl.MouseClickMenu.Disable();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    // 마우스 오른쪽 버튼 누를 시 마우스 포인터 위치에 속성 메뉴 활성화
+    private void OnElemanterMenu(InputAction.CallbackContext context)
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+
+        elemanterMenu.position = mousePos;
+        elemanterMenu.gameObject.SetActive(true);
+
+    }
+
+    // 속성 메뉴 활성화 된 상태에서 속성메뉴 위치에서 마우스 오른쪽 버튼 해제 시 속성 선택
+    private void OffElemanterMenu(InputAction.CallbackContext context)
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        Vector3 target = context.ReadValue<Vector2>();
+        target = mousePos;
+
+        if (target.y > elemanterMenu.position.y)
+        {
+            fire.OnClickBuuton();
+            testPlayer.element = Element.Fire;
+            elemanterMenu.gameObject.SetActive(false);
+        }
+        else if (target.x > elemanterMenu.position.x)
+        {
+            water.OnClickBuuton();
+            testPlayer.element = Element.Water;
+            elemanterMenu.gameObject.SetActive(false);
+        }
+        else if (target.y < elemanterMenu.position.y)
+        {
+            wind.OnClickBuuton();
+            testPlayer.element = Element.Wind;
+            elemanterMenu.gameObject.SetActive(false);
+        }
+        else if (target.x < elemanterMenu.position.x)
+        {
+            thunder.OnClickBuuton();
+            testPlayer.element = Element.Thunder;
+            elemanterMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            testPlayer.element = Element.None;
+            elemanterMenu.gameObject.SetActive(false);
+        }
+
+
     }
 }

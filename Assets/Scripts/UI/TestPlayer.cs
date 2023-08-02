1ec829daa1f3e1115176ef2b7bbe11579bbbafff
kim su young <qpwlwkd@naver.com>
2023-08-02 15:20:36 +0900

    ui 수정

--- Assets/Scripts/UI/TestPlayer.cs
+++ Assets/Scripts/UI/TestPlayer.cs
@@ -5,16 +5,8 @@ using UnityEngine.InputSystem.Switch;
 
 public class TestPlayer : MonoBehaviour
 {
-    int hp = 100;
-    public int HP
-    {
-        get { return hp; }
-        set { hp = value; }
-    }
 
-    public Element element;
-
-    }
+}
 
  
     

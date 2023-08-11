8fc015354fc5bb22ed9fc2bfab271d70a0098e31
kim su young <may721@naver.com>
2023-08-11 15:20:12 +0900

    ui 수정

---
+++ Assets/Scripts/Character/Elemantal/ElemantalStatus.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Elemantal
+{
+    None,
+    Fire,
+    Water,
+    Wind,
+    Thunder
+}
+public class ElemantalStatus 
+{
+   public  Elemantal elemantal = Elemantal.None;
+
+    public int elemantalLevel = 0;
+
+
+}

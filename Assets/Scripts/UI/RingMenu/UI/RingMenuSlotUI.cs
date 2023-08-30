1c12ae25ca52d663532e806a469e71a3b142688a
kim su young <may721@naver.com>
2023-08-30 15:20:59 +0900

    ui 수정

--- Assets/Scripts/UI/RingMenu/UI/RingMenuSlotUI.cs
+++ Assets/Scripts/UI/RingMenu/UI/RingMenuSlotUI.cs
@@ -10,45 +10,21 @@ public class RingMenuSlotUI : MonoBehaviour, IPointerClickHandler  // 포인터 이�
 
     public System.Action<uint>  onClick;
 
-    uint index = 0; 
-    private void Awake()
+
+    protected virtual void Awake()
     {
         onClick= null;
         elementalType = ElementalType.None;
     }
 
-    public void SlotSelect()
-    {
-        switch (elementalType)
-        {
-            case ElementalType.Fire:
-                index = 1;
-                
-                break;
-            case ElementalType.Wind:
-                index = 2;
-                break;
-            case ElementalType.Water:
-                index = 3;
-                break;
-            case ElementalType.Thunder:
-                index = 4;
-                break;
 
-        }
-    }
 
     // 이벤트 발생하는 인터페이스 함수
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        GameObject obj = eventData.pointerClick.gameObject;
-
-        RingMenuSlotUI clickSlot = obj.GetComponent<RingMenuSlotUI>();
-        if(clickSlot != null)
-        {
-            onClick?.Invoke(clickSlot.index);
-            Debug.Log($"드래그 종료 : {clickSlot.index}번 슬롯");
-        }
+        onClick?.Invoke((uint)elementalType);
     }
+
+  
 }
--- Assets/Scripts/UI/RingMenu/UI/RingMenuUI.cs
+++ Assets/Scripts/UI/RingMenu/UI/RingMenuUI.cs
@@ -8,36 +8,38 @@ using TMPro;
 using System;
 using Unity.VisualScripting;
 
-public class RingMenuUI : MonoBehaviour
+public class RingMenuUI : RingMenuSlotUI
 {
     ActionControl acionControl;
-
     // 속성 메뉴
     Transform elemanterMenu;
 
-    RingMenuSlotUI menuSlotUI;
+    RingMenuSlotUI[] slot;
 
-
-
-   
-
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         acionControl = new ActionControl();
 
-       elemanterMenu = transform.GetChild(0);
+        elemanterMenu = transform.GetChild(0);
+        slot = new RingMenuSlotUI[5];
+        slot[0] = elemanterMenu.GetChild(0).GetComponent<RingMenuSlotUI>();
+        slot[1] = elemanterMenu.GetChild(1).GetComponent<RingMenuSlotUI>();
+        slot[2] = elemanterMenu.GetChild(2).GetComponent<RingMenuSlotUI>();
+        slot[3] = elemanterMenu.GetChild(3).GetComponent<RingMenuSlotUI>();
        
-        menuSlotUI = new RingMenuSlotUI();
-        
+        slot[0].onClick += SlotSelect;
+        slot[1].onClick += SlotSelect;
+        slot[2].onClick += SlotSelect;
+        slot[3].onClick += SlotSelect;
         
        
     }
+
     private void OnEnable()
     {
         acionControl.MouseClickMenu.Enable();
-        acionControl.MouseClickMenu.MouseLeft.performed += ElemantalMove;
-        acionControl.MouseClickMenu.MouseLeft.canceled += ElemantalMove;
-
         acionControl.MouseClickMenu.MouesRight.performed += OnRingMenu;
         acionControl.MouseClickMenu.MouesRight.canceled += RingSlotSelect;
     }
@@ -45,8 +47,8 @@ public class RingMenuUI : MonoBehaviour
 
     private void OnDisable()
     {
-        acionControl.MouseClickMenu.MouseLeft.canceled -= ElemantalMove;
-        acionControl.MouseClickMenu.MouseLeft.performed -= ElemantalMove;
+        acionControl.MouseClickMenu.MouesRight.canceled -= RingSlotSelect;
+        acionControl.MouseClickMenu.MouesRight.performed -= OnRingMenu;
         acionControl.MouseClickMenu.Disable();
     }
 
@@ -55,19 +57,6 @@ public class RingMenuUI : MonoBehaviour
         elemanterMenu.gameObject.SetActive(false);
     }
 
-    private void Update()
-    {
-        
-    }
-
-    /// <summary>
-    /// 속성을 RingMenu에 옮길때 사용
-    /// </summary>
-    /// <param name="context"></param>
-    private void ElemantalMove(InputAction.CallbackContext context)
-    {
-        
-    }
 
     /// <summary>
     ///  오른쪽 마우스로 RingMenu 호출
@@ -91,9 +80,35 @@ public class RingMenuUI : MonoBehaviour
         elemanterMenu.gameObject.SetActive(false);
     }
 
-    private void OnSlotClick(uint index)
+    public void SlotClick()
     {
-       
+        
     }
+   
+
+    public void SlotSelect(uint index)
+    {
+        switch (index)
+        {
+            case (uint)ElementalType.Fire:
+          
+                Debug.Log("불");
+                break;
+            case (uint)ElementalType.Wind:
+                
+                Debug.Log("바람");
+                break;
+            case (uint)ElementalType.Water:
+              
+                Debug.Log("물");
+                break;
+            case (uint)ElementalType.Thunder:
+             
+                Debug.Log("번개");
+                break;
+
+        }
+    }
+
 
 }

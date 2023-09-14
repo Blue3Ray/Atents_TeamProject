409fcd17a067e6474b28b2e5f8472b3a7cfeaf25
Blue3Ray <xoals1126@gmail.com>
2023-09-14 15:20:01 +0900

    BoneEnemy 업데이트
    공격 쿨타임을 적용하였고 공격이 끝나고 쿨타임이 남아 있을때도 정상적으로 플레이어를 추적하게하였음.
    추적하려는 플레이어와 x값이 비슷할 때 좌우 잔상효과가 보이는 버그를 제거

--- Assets/Scripts/Enemy/BoneEnemy.cs
+++ Assets/Scripts/Enemy/BoneEnemy.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoneEnemy : MonoBehaviour
 {
     // 공격 기능 ---------------------------------------------------------------
 
+    /// <summary>
+    /// 공격 쿨다운
+    /// </summary>
     float attackMaxCoolTime = 3.0f;
-    float attackCurrentCoolTime = 3.0f;
+    /// <summary>
+    /// 공격 쿨다운 남은 시간(초기값은 0보다 작아야 이후 공격 쿨다운이 작동됨, 양수면 작동 안됨)
+    /// </summary>
+    [SerializeField]
+    float attackCurrentCoolTime = -1f;
+
+    /// <summary>
+    /// 공격 쿨다운 코루틴을 저장할 변수
+    /// </summary>
+    IEnumerator attackCoolDown;
 
     /// <summary>
     /// 공격 범위
@@ -18,6 +31,12 @@ public class BoneEnemy : MonoBehaviour
     /// 공격할 대상
     /// </summary>
     CharacterBase attackTarget;
+
+    float attackRange;
+    /// <summary>
+    /// 공격 애니메이션 도중인지 확인하는 변수(상태머신이랑 별개로 움직임)
+    /// </summary>
+    bool isAttack = false;
 
     // 이동 기능 ---------------------------------------------------------------
     public float maxMoveSpeed;
@@ -124,18 +143,18 @@ public class BoneEnemy : MonoBehaviour
                         WaitTimer = waitTime;           // 대기 상태 들어가면 최대 대기시간 설정
                         onStateUpdate = Update_Wait;
                         break;
-                    case EnemyState.Patrol:             // Wait, Patrol, Chase는 Speed에 따라서 애니메이션 바뀜
+                    case EnemyState.Patrol:             // Wait, (Patrol)는 Speed에 따라서 애니메이션 바뀜
                         CurrentMoveSpeed = maxMoveSpeed;
                         onStateUpdate = Update_Patrol;
                         break;
-                    case EnemyState.Chase:
-                        CurrentMoveSpeed = maxMoveSpeed;
+                    case EnemyState.Chase:              // CurrentMove에 따라서 애니메이션 바뀌기 때문에 추적 내부에 이동 속도를 조절함
+                        //CurrentMoveSpeed = maxMoveSpeed;
                         onStateUpdate = Update_Chase;
                         break;
                     case EnemyState.Attack:
+
                         CurrentMoveSpeed = 0;
-                        animator.SetTrigger(Hash_IsAttack);
-                        attackCurrentCoolTime = attackMaxCoolTime;
+                        
                         onStateUpdate = Update_Attack;
                         break;
                     case EnemyState.Dead:
@@ -160,7 +179,6 @@ public class BoneEnemy : MonoBehaviour
     readonly int Hash_Speed = Animator.StringToHash("Speed");
     readonly int Hash_IsAttack = Animator.StringToHash("IsAttack");
 
-
     Rigidbody2D rb;
 
 
@@ -171,26 +189,34 @@ public class BoneEnemy : MonoBehaviour
 
         AttackArea[] areas = GetComponentsInChildren<AttackArea>();
         attackArea = areas[0];
+
+        //공격 범위에 들어왔을 때
         attackArea.onPlayerIn += (target) =>
         {
             if (State == EnemyState.Chase)       // 추적 상태 였을 때
             {
                 attackTarget = target;          // 공격 대상 지정
-                State = EnemyState.Attack;      // 공격 상태로 변경
+                if(attackCurrentCoolTime < 0) State = EnemyState.Attack;      // 공격 상태로 변경
             }
         };
 
+        //공격 범위에서 나갔을 때
         attackArea.onPlayerOut += (target) =>
         {
             if (attackTarget == target)          // 공격 대상이 나가면 
             {
                 attackTarget = null;            // 공격 대상 초기화
-                if (State != EnemyState.Dead)    // 죽은 상태가 아니면
+
+                if (State != EnemyState.Dead && !isAttack)    // 죽은 상태가 아니고, 공격 중이 아닐 때
                 {
                     State = EnemyState.Chase;   // 추적 상태로 변경
                 }
             }
         };
+
+        attackRange = Mathf.Abs(transform.position.x - attackArea.transform.position.x);
+
+        attackCoolDown = AttackCoolDown();
 
         OnInitialize();
     }
@@ -231,7 +257,7 @@ public class BoneEnemy : MonoBehaviour
         else
         {
             rb.MovePosition(transform.position + (Vector3) (CurrentMoveSpeed * Time.deltaTime * 0.5f * MoveDir));
-            //transform.Translate(CurrentMoveSpeed * Time.deltaTime * dir);
+            
             if (Mathf.Abs(waypoints[currentWaypointIndex].x - transform.position.x) < 0.1f)
             {
                 currentWaypointIndex++;
@@ -242,39 +268,97 @@ public class BoneEnemy : MonoBehaviour
         }
     }
 
+    float currentChaseTime = 0.3f;
+    float ChaseMaxUpdateTime = 0.3f;
+
     void Update_Chase()
     {
-        if(SearchPlayer())
+        // 플레이어와 x값이 곂칠 때 좌우 잔상효과를 제거하기 위한 움직임 업데이트 최소 시간 값 부여
+        if (currentChaseTime < 0)
         {
-            // 타켓한테 다가가기
-            Vector3 targetPos = chaseTarget.transform.position;
-
-            if(targetPos.x > transform.position.x)
+            if (SearchPlayer())
             {
-                MoveDir = new Vector2(1, 0);
+                // 타켓한테 다가가기
+                Vector3 targetPos = chaseTarget.transform.position;
+
+                if (targetPos.x > transform.position.x)
+                {
+                    MoveDir = new Vector2(1, 0);
+                }
+                else
+                {
+                    MoveDir = new Vector2(-1, 0);
+                }
+
+                // 근접 거리면 대기하기
+                if (Mathf.Abs(targetPos.x - transform.position.x) < attackRange)
+                {
+                    CurrentMoveSpeed = 0;
+                }
+                else
+                {
+                    CurrentMoveSpeed = maxMoveSpeed;
+                }
+                
             }
             else
             {
-                MoveDir = new Vector2(-1, 0);
+                // 타켓 대상을 잃어버릴 때 대기 상태로 돌입
+                State = EnemyState.Wait;
             }
-            rb.MovePosition(transform.position + (Vector3)(CurrentMoveSpeed * Time.deltaTime * moveDir));
+            currentChaseTime = ChaseMaxUpdateTime;
         }
-        else
-        {
-            // 타켓 대상을 잃어버릴 때 대기 상태로 돌입
-            State = EnemyState.Wait;
-        }
+
+        rb.MovePosition(transform.position + (Vector3)(CurrentMoveSpeed * Time.deltaTime * moveDir));
+        currentChaseTime -= Time.deltaTime;
     }
 
     void Update_Attack()
     {
-        attackCurrentCoolTime -= Time.deltaTime;
-        
         if (attackCurrentCoolTime < 0)
         {
             animator.SetTrigger(Hash_IsAttack);
+            StopCoroutine(attackCoolDown);
             attackCurrentCoolTime = attackMaxCoolTime;
+            StartCoroutine(attackCoolDown);
             //(attackTarget);
+        }
+    }
+
+    IEnumerator AttackCoolDown()
+    {
+        while (true)        // 코루틴을 변수에 저장하게 되면 Stop하고 다시 시작 할 때 처음부터가 아닌 중단한 곳부터 시작하게되기 때문에 무한 루프 붙여줌
+        {
+            while (attackCurrentCoolTime > 0)
+            {
+                attackCurrentCoolTime -= Time.deltaTime;
+                yield return null;
+            }
+            yield return null;
+            if(attackTarget != null) State = EnemyState.Attack;
+        }
+    }
+
+    /// <summary>
+    /// 공격이 시작할 때 호출 되는 함수
+    /// </summary>
+    void AttackStart()
+    {
+        isAttack = true;
+    }
+
+    /// <summary>
+    /// 공격이 끝날 때 호출되는 함수(공격하는 도중 방향 전환 안되게 할려고함)
+    /// </summary>
+    void AttackEnd()
+    {
+        isAttack = false;
+        if (attackTarget == null)
+        {
+            if (State != EnemyState.Dead)    // 죽은 상태가 아니면
+            {
+                State = EnemyState.Chase;   // 추적 상태로 변경
+            }
         }
     }
 
@@ -284,8 +368,25 @@ public class BoneEnemy : MonoBehaviour
     }
 
 
-    // ---------------------------------
+    // 공격 기능 ---------------------------------------
 
+    /// <summary>
+    /// 공격 애니메이션에 달릴 이벤트 함수
+    /// </summary>
+    void AttackTargetInArea()
+    {
+        if (attackTarget != null)
+        {
+            Attack(attackTarget);
+        }
+    }
+
+    void Attack(CharacterBase target)
+    {
+        target.Defence(10, 10 * MoveDir.x);
+    }
+
+    // 감지 기능---------------------------------------------
     bool SearchPlayer()
     {
         bool result = false;
@@ -307,7 +408,7 @@ public class BoneEnemy : MonoBehaviour
             }
             else
             {
-                // 근접 범위 밖이면
+                // 근접 범위 밖이면(사이에 벽이 있을 때 구분해야됨)
                 chaseTarget = target.transform;
                 result = true;
             }
